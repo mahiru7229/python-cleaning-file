378a7fa4918a44f0deb9827ed50ecef41581ba2e
mahiru7229 <110292918+mahiru7229@users.noreply.github.com>
2023-07-26 23:42:11 +0700

    Hoàn thành

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# python-cleaning-file
- 
+# Xin chào
+Đây là phần mềm giúp các bạn dọn dẹp đống file bừa bộn mà bạn quá nhát để có thể sắp xếp vào đúng vị trí của nó :D
+
+# Cách sử dụng
+
+Bạn có thể xem [tại đây](https://github.com/mahiru7229/python-cleaning-file/wiki/Cách-để-dùng-phần-mềm).
+
+# Contribute 
+**mahiru7229**, **customtkinter**.
--- main.py
+++ main.py
@@ -8,12 +8,62 @@ import webbrowser
 FONT = ("Comic Sans MS", 25)
 FONT_BUTTON = ("Comic Sans MS", 20)
 
+#cho trường hợp mà người mới mới tải file về mà chưa có file extension.json thì phần mềm sẽ tự tạo ra một file default mới như vậy: 
+extensions_main = {
+    ".txt": "Text File",
+    ".doc": "Microsoft Word Document",
+    ".docx": "Microsoft Word Document",
+    ".xls": "Microsoft Excel Spreadsheet",
+    ".xlsx": "Microsoft Excel Spreadsheet",
+    ".ppt": "Microsoft PowerPoint Presentation",
+    ".pptx": "Microsoft PowerPoint Presentation",
+    ".pdf": "Adobe Portable Document Format",
+    ".jpg": "JPEG Image",
+    ".jpeg": "JPEG Image",
+    ".png": "Portable Network Graphics Image",
+    ".gif": "Graphics Interchange Format Image",
+    ".bmp": "Bitmap Image",
+    ".mp3": "MP3 Audio File",
+    ".wav": "WAV Audio File",
+    ".mp4": "MP4 Video File",
+    ".avi": "Audio Video Interleave Video File",
+    ".mkv": "Matroska Video File",
+    ".zip": "Zip Archive",
+    ".rar": "RAR Archive",
+    ".7z": "7-Zip Archive",
+    ".exe": "Executable File",
+    ".apk": "Android Package File",
+    ".iso": "Disk Image File",
+    ".csv": "Comma-Separated Values File",
+    ".xml": "Extensible Markup Language File",
+    ".json": "JavaScript Object Notation File",
+    ".html": "Hypertext Markup Language File",
+    ".htm": "Hypertext Markup Language File",
+    ".css": "Cascading Style Sheet File",
+    ".js": "JavaScript File",
+    ".py": "Python Script File",
+    ".cpp": "C/C++ Source Code File",
+    ".c": "C/C++ Source Code File",
+    ".java": "Java Source Code File",
+    ".php": "PHP Script File",
+    ".asp": "Active Server Page File",
+    ".aspx": "Active Server Page File"
+}
+if not os.path.exists("extension.json"):
+    with open("extension.json", "w") as f:
+        json.dump(extensions_main, f, indent=4)
+        f.close()
+if not os.path.exists("need_to_clean"):
+    os.mkdir("need_to_clean")
 
+
+#Trang report
+def report():
+    webbrowser.open("https://github.com/mahiru7229/python-cleaning-file/issues")
 
 #Mở trang hướng dẫn sử dụng
-def open_help_center():
-    webbrowser.open("google.com")
-
+def open_help_center():    
+    webbrowser.open("https://github.com/mahiru7229/python-cleaning-file/wiki/Cách-để-dùng-phần-mềm")
 #Phần code để dọn dẹp file
 def cleaning():
     if os.path.exists("need_to_clean"):
@@ -26,8 +76,11 @@ def cleaning():
                     last_dir = directory + "\\" + i
                     move_dir = directory + "\\" + extensions[j]
                     move_file(last_dir,move_dir)
+        messagebox.showinfo(title="Hoàn thành", message="Đã dọn dẹp tất cả các file trong thư mục vào các thư mục riêng biệt !")
     else:
-        messagebox.showerror(title="Lỗi", message='Chưa tạo folder với tên "need_to_clean" bên cạnh phần mềm !')
+        os.mkdir("need_to_clean")
+        messagebox.showerror(title="Lỗi", message='Folder với tên "need_to_clean" không tồn tại !\nPhần mềm sẽ tự tạo lại folder!')
+
 
 #Hàm của phần mềm
 def create_folder(dir):
@@ -59,5 +112,10 @@ help_button.grid(row= 0, column = 1,padx = 10, pady = 10)
 move_button = customtkinter.CTkButton(windows, text = "Dọn dẹp file", font =FONT_BUTTON,command=cleaning)
 move_button.grid(row= 1, column = 1,padx = 10, pady = 10)
 
+move_button = customtkinter.CTkButton(windows, text = "Report lỗi phần mềm", font =FONT_BUTTON,command=report)
+move_button.grid(row= 2, column = 1,padx = 10, pady = 10)
+
+
+windows.title("File Cleaner 1.0 :D")
 windows.resizable(width=False, height=False)
 windows.mainloop()
